475a2e1a0a2e810b776b17bf713e735c974fc527
vasil <vaskonikolov2003@gmail.com>
2022-08-16 19:47:08 +0300

    improve structure

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from src.point_and_shoot import point
-
+from point_and_shoot import point, shoot
 
 def main():
-    point.point(1) 
-    return 1
+    location = (0, 0) 
+    point.point(location)
+    image = shoot.take_picture()
+    
 
 if __name__ == "__main__":
     main()
---
+++ src/point_and_shoot/shoot.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+class SatImage:
+    def __init__(self, image, metadata):
+        self.image = image 
+        self.metadata = metadata
+
+def take_picture() -> SatImage:
+    """
+    A fucntion which interfaces with the camera of the satellite and returns a picture. This function should be called right after the point
+    function. 
+    Args: TBD 
+    returns: An image and its metadata, TBD
+    """ 
+    # TODO implement
+    return SatImage(1, 1) 
+
+
+

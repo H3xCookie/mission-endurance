593024ae95c35b36b2a255b5e4cb39ad2631024f
vasil <vaskonikolov2003@gmail.com>
2022-08-18 16:54:29 +0300

    show true color of the images

--- src/superimpose/superimpose.py
+++ src/superimpose/superimpose.py
@@ -1,7 +1,7 @@
 from point_and_shoot.shoot import SatImage 
+import numpy as np
 import geopandas as gpd
-import rasterio as rio
-from rasterio.plot import show
+import rasterio, rasterio.plot
 import matplotlib.pyplot as plt
 import os
 
@@ -11,21 +11,48 @@ def filter_image(original_image: SatImage, shapefile) -> SatImage:
     has a color (0, 0, 0). 
     returns: SatImage which has been filtered of the pixels outside of the field 
     """
-    # TODO 
     shapefile_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "farm_shapefiles.zip")
     data = gpd.read_file(shapefile_filename)
-    image_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "Clipped_Bulgaria.tif")
-    image = rio.open(image_filename)
-    # works only for EPSG coordinate system
-    image_crs_index = int(str(image.crs).split(":")[1])
-    data = data.to_crs(image_crs_index)
-    print(data.crs, image.crs)
 
-    # show the image and the shapefiles
-    fig, ax = plt.subplots()
-    bounds = [image.bounds[0], image.bounds[2], image.bounds[1], image.bounds[3]]
-    ax = rio.plot.show(image, extent=bounds, ax=ax, cmap='pink')
-    data.plot(ax=ax)
-    plt.show()
-    return original_image
+    print(data["geometry"][0])
+    image_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "Clipped_Bulgaria.tif")
+    modified_image_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "Clipped_Bulgaria_modified.tif")
+
+    # make the image uint8, with max value 255
+    with rasterio.open(image_filename, "r+") as image:
+        profile = image.profile
+        profile.update(
+            dtype=rasterio.uint8,
+        )
+        image_data = image.read((3, 2, 1))
+
+        max_value = image_data.max()
+        print(image_data.min(), image_data.max())
+        image_data = image_data / (max_value/254)
+        image_data = image_data.astype(np.uint8)
+        print(image_data.min(), image_data.max())
+
+    with rasterio.open(modified_image_filename, "w", **profile) as mod_image:
+        mod_image.write(image_data)
+        pass
+
+    with rasterio.open(modified_image_filename) as image:
+        # works only for EPSG coordinate system
+        image_crs_index = int(str(image.crs).split(":")[1])
+        data = data.to_crs(image_crs_index)
+        polygons = list(data["geometry"])
+        interesting_polygon = polygons[0]
+
+        
+        # show the image and the shapefiles
+        fig, ax = plt.subplots()
+        bounds = [image.bounds[0], image.bounds[2], image.bounds[1], image.bounds[3]]
+        
+        ax = rasterio.plot.show(image, extent=bounds, ax=ax)
+        our_series = gpd.GeoSeries([interesting_polygon])
+        # data["geometry"].plot(ax=ax)
+        our_series.plot(ax=ax)
+        plt.show()
+        return original_image
+
 

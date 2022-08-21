bad89da38b3bde2128673ea52893d937cb33b5de
vasil <vaskonikolov2003@gmail.com>
2022-08-21 19:40:49 +0300

    spaghetti but it works

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
+import sys
 import os
 import geopandas as gpd
 from point_and_shoot import point, shoot
 from superimpose import superimpose
 
 def main():
+    """
+    pass the folder which contains the merged.tif file, without the _PROCESSED part, as an argument to main
+    """
+    folder = sys.argv[1].split(".")[0]
+    image_location = os.path.join(os.getcwd(), "OUTPUT_TIF", f"{folder}_PROCESSED", "merged.tif")
     location = (0, 0)
-    # point.point(location)
-    # image = shoot.take_picture()
+    point.point(location)
+    image = shoot.take_picture_from_file(image_location)
 
     shapefile_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "farm_shapefiles.zip")
     data = gpd.read_file(shapefile_filename)
--- src/point_and_shoot/shoot.py
+++ src/point_and_shoot/shoot.py
@@ -2,6 +2,39 @@ from .sat_image import SatImage
 import os
 import numpy as np
 import rasterio
+def take_picture_from_file(filename) -> SatImage:
+    """
+    opens a .tif GeoTIFF image 
+    returns: An image and its metadata, TBD
+    """ 
+    # image_filename = os.path.join(
+    #     "/", 
+    #     "home", "vasil", "mission-endurance", "Output",
+    #     "S2B_MSIL2A_20220731T085559_N0400_R007_T35TNH_20220731T103425_PROCESSED", 
+    #     "merged.tif"
+    # )
+    
+    modified_image_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "Clipped_Bulgaria_modified.tif")
+
+    # make the image uint8, with max value 255
+    with rasterio.open(filename, "r+") as image:
+        new_profile = image.profile
+        new_profile.update(
+            dtype=rasterio.uint8,
+            count=3
+        )
+        image_data = image.read((2, 3, 4))
+
+        max_value = image_data.max()
+        image_data = image_data / (max_value/254.99)
+        image_data = image_data.astype(np.uint8)
+        print(image_data.shape)
+
+    with rasterio.open(modified_image_filename, "w", **new_profile) as mod_image:
+        mod_image.write(image_data)
+        pass
+     
+    return SatImage(filename=modified_image_filename) 
 
 def take_picture() -> SatImage:
     """

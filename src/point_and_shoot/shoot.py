9ac58038c8cbc696fd8cc409ebe2c99e3da83b02
vasil <vaskonikolov2003@gmail.com>
2022-08-21 19:21:01 +0300

    add correct profile for band-filtered image

--- src/point_and_shoot/shoot.py
+++ src/point_and_shoot/shoot.py
@@ -10,22 +10,29 @@ def take_picture() -> SatImage:
     Args: TBD 
     returns: An image and its metadata, TBD
     """ 
-    image_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "Clipped_Bulgaria.tif")
+    image_filename = os.path.join(
+        "/", 
+        "home", "vasil", "mission-endurance", "Output",
+        "S2B_MSIL2A_20220731T085559_N0400_R007_T35TNH_20220731T103425_PROCESSED", 
+        "merged.tif"
+    )
     modified_image_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "Clipped_Bulgaria_modified.tif")
 
     # make the image uint8, with max value 255
     with rasterio.open(image_filename, "r+") as image:
-        profile = image.profile
-        profile.update(
+        new_profile = image.profile
+        new_profile.update(
             dtype=rasterio.uint8,
+            count=3
         )
-        image_data = image.read((3, 2, 1))
+        image_data = image.read((2, 3, 4))
 
         max_value = image_data.max()
         image_data = image_data / (max_value/254.99)
         image_data = image_data.astype(np.uint8)
+        print(image_data.shape)
 
-    with rasterio.open(modified_image_filename, "w", **profile) as mod_image:
+    with rasterio.open(modified_image_filename, "w", **new_profile) as mod_image:
         mod_image.write(image_data)
         pass
      

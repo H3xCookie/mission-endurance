93d1688cb71db0efb2860ac8c179fdc83781b7c9
vasil <vaskonikolov2003@gmail.com>
2022-08-21 20:05:18 +0300

    the script only converts to tif and stops

--- sent_to_tif.sh
+++ sent_to_tif.sh
@@ -1,11 +1,8 @@
-# ./clean.sh
-# ./download.sh
+./clean.sh
+./download.sh
 # convert .safe format of sentinel_2 sats to .tif GeoTIFF
 file=$(ls ./PRODUCT)
 echo $file
-# python3 ./Sentinel-Scripts-master/sentinel_2/tiff-generator.py ${file} 
-
-# run the main script
-python3 ./src/main.py ${file} 
+python3 ./Sentinel-Scripts-master/sentinel_2/tiff-generator.py ${file} 
 
 

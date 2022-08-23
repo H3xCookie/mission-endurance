be176c073fedade431312ef32b345c7ee2907b8a
vasil <vaskonikolov2003@gmail.com>
2022-08-23 15:42:14 +0300

    add bands to MLModelInput

--- src/main.py
+++ src/main.py
@@ -16,10 +16,12 @@ def main():
 
     pointing_location = (0, 0)
     point.point(pointing_location)
-    image = shoot.take_bands(image_location, (4, 3, 2))
+    bands = (4, 3, 2)
+    image = shoot.take_picture_from_file(image_location, bands)
 
     shapefile_filename = os.path.join("/", "home", "vasil", "mission-endurance", "data", "farm_shapefiles.zip")
     data = gpd.read_file(shapefile_filename)
+
     filtered_image = superimpose.filter_image(image, data)
 
 if __name__ == "__main__":
--- src/superimpose/superimpose.py
+++ src/superimpose/superimpose.py
@@ -7,36 +7,34 @@ import matplotlib.pyplot as plt
 import os
 
 class MLModelInput:
-    def __init__(self, data):
+    def __init__(self, bands, data):
         """
+        bands: a tuple of the band numbers in sentinel notation. len(bands) = n_channels. Ex. bands = (4, 3, 2) means R, R, B
         data: np.ndarray of shape (n_channels, height, width). If n_channels=3 they are R, G, B
         """
+        self.bands = bands
         self.data = data
 
-def filter_image(original_image: SatImage, shapefile: gpd.GeoDataFrame) -> MLModelInput:
+def filter_image(band_filtered_image: SatImage, shapefile: gpd.GeoDataFrame) -> MLModelInput:
     """
     takes a SatImage object and a shapefile corresponding to a field on that image, and makes it so that everything outisde the shapefile
     has a color (0, 0, 0). 
-    original_image: An SatImage wrapper of an already created .tif image, with correct metadata and in RGB format (ch. 1 is R, ch. 2 is G,
-    ch. 3 is B)
+    band_filtered_image: An SatImage wrapper of an already created .tif image, with correct metadata and filtered by useful bands 
     shapefile: shapefile read by `gpd.read_file(<path_to_shapefile>.zip)`
     returns: SatImage which has been filtered of the pixels outside of the field 
     """
-    image = original_image.image # rasterio image
     # works only for EPSG coordinate system
-    original_image_crs_index = int(str(image.crs).split(":")[1])
+    band_filtered_image_crs_index = int(str(band_filtered_image.image.crs).split(":")[1])
     # make shapefile to be in the same coordinate system as image
-    data = gpd.GeoDataFrame(shapefile.to_crs(original_image_crs_index))
+    data = gpd.GeoDataFrame(shapefile.to_crs(band_filtered_image_crs_index))
 
-    rio_image = rioxarray.open_rasterio(image)
+    cropped_image = rioxarray.open_rasterio(band_filtered_image.image).rio.clip(data["geometry"][[1]], data.crs)
      
-    cropped_image = rio_image.rio.clip(data["geometry"][[1]], data.crs)
-
     fig, ax = plt.subplots()
     # rio_image.plot.imshow(ax=ax)
 
     # data["geometry"].plot(ax=ax)
     cropped_image.plot.imshow(ax=ax)
     plt.show()
-    return MLModelInput(cropped_image.data)
+    return MLModelInput(band_filtered_image.bands, cropped_image.data)
 

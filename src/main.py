56b3021cb969b21ec8bbb0080f411eb81b9dbb6a
vasil <vaskonikolov2003@gmail.com>
2022-08-29 15:46:46 +0300

    first working version of the satellite main fn

--- src/main.py
+++ src/main.py
@@ -3,21 +3,15 @@ import os
 import sys
 
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-from preprocessing import cloud_mask
-from processing import compute_coastline, correlate_images
+from preprocessing import cloud_mask, precompute_coastline
+from processing import compute_coastline, correlate_images, crop_field
 from time_and_shoot import shoot
 from time_and_shoot.sat_image import SatImage
-from superimpose import superimpose
-
-
-def precompute_coastline():
-    base_image = cv2.imread("./monkedir/base_image_example.tiff")
-    final_image_data = compute_coastline.compute_coastline(SatImage(image=base_image))
-    final_image_data = final_image_data.data.astype(np.uint8) * 255
-    cv2.imwrite("./monkedir/precomputed_coastline.tiff", final_image_data)
+from image_analysis import indeces, make_decision
+from communications import downlink
 
 
 def sat_main():
@@ -25,6 +19,12 @@ def sat_main():
     the main fn which runs on the satellite. fiedl coords must
     be in the form (x, y), and be in counter-clockwise direction in the coordinate system of the image(x right, y down).
     """
+    # take picture
+    time_to_take_picture = "2022:09:03,12:00:00,000"
+    sat_image = shoot.take_picture(time_to_take_picture)
+    sat_image = cloud_mask.mask_clouds(sat_image)
+    sat_coastline = compute_coastline.compute_coastline(sat_image)
+
     points = [[462, 210], [469, 276], [525, 266], [516, 217]]
     # needs to be of shape (n, 1, 2) to be able to be acted on by homography
     field_coords_px = np.array(points).reshape((len(points), 1, 2))
@@ -35,42 +35,40 @@ def sat_main():
     parser.add_argument("--computed_coastline", required=True)
     args = parser.parse_args()
 
-    computed_coastline = SatImage(
-        image=cv2.cvtColor(
-            cv2.imread(args.computed_coastline) * 255, cv2.COLOR_BGR2GRAY
-        )
+    computed_coastline = precompute_coastline.load_precomputed_coastline(
+        args.computed_coastline
     )
-
-    # take picture
-    time_to_take_picture = "2022:09:03,12:00:00,000"
-    sat_image = shoot.take_picture(time_to_take_picture)
-    sat_image = cloud_mask.mask_clouds(sat_image)
-    sat_coastline = compute_coastline.compute_coastline(sat_image)
 
     # compute and apply homography to the original sat image
     homography = correlate_images.compute_affine_transform(
         computed_coastline, sat_coastline
     )
-    h, w = sat_image.data.shape[:2]
+    # h, w = sat_image.data.shape[:2]
+    base_h, base_w = computed_coastline.data.shape[:2]
     aligned_image = SatImage(
         image=cv2.warpPerspective(
-            sat_image.data, homography, (w, h), flags=cv2.INTER_NEAREST
+            sat_image.data, homography, (base_w, base_h), flags=cv2.INTER_NEAREST
         )
     )
 
     # pass aligned image and coordinates to image recognition algorithm
-    polygon = superimpose.Polygon(field_coords_px.reshape((len(points), 2)))
-    field_mask = superimpose.filter_polygon(aligned_image.data, polygon)
-    print("field mask shape", field_mask.shape)
-    print("sat image shape", sat_image.data.shape)
-    filtered_image = aligned_image.data * field_mask[:, :, np.newaxis]
-    print(filtered_image.shape)
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-    base_image = cv2.imread("./monkedir/base_image_example.tiff")
-    ax1.imshow(base_image)
-    ax2.imshow(aligned_image.data)
-    ax3.imshow(filtered_image)
-    plt.show()
+    polygon = crop_field.Polygon(field_coords_px.reshape((len(points), 2)))
+    field_mask = crop_field.filter_polygon(aligned_image.data, polygon)
+    filtered_image = SatImage(image=aligned_image.data * field_mask[:, :, np.newaxis])
+    only_field = crop_field.crop_filtered_image(filtered_image)
+
+    # compute the Green index of the field
+    green_index = indeces.green_index(only_field)
+
+    downlink.send_message_down(
+        f"{green_index}: {make_decision.is_field_planted(green_index)}"
+    )
+    # fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    # base_image = cv2.imread("./monkedir/base_image_example.tiff")
+    # ax1.imshow(base_image)
+    # ax2.imshow(aligned_image.data)
+    # ax3.imshow(only_field.data)
+    # plt.show()
 
 
 def presentation_images():
@@ -142,6 +140,6 @@ def presentation_images():
 
 
 if __name__ == "__main__":
-    # precompute_coastline()
+    # precompute_coastline.precompute_coastline()
     sat_main()
     # presentation_images()

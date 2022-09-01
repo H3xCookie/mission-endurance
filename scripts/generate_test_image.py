72996265abc7c1215f59550f21524152892c074f
vasil <vaskonikolov2003@gmail.com>
2022-09-01 17:09:59 +0300

    try out different indeces,create tests

---
+++ scripts/generate_test_image.py
@@ -0,0 +1,27 @@
+import rasterio
+
+
+def shift_given_image():
+    filename = "./monkedir/stacked_rgb.tiff"
+    offset_filename = "./monkedir/offset_stacked_rgb.tiff"
+    with rasterio.open(filename) as base_image:
+        data = base_image.read()
+        height, width = data.shape[1:]
+        meta = base_image.meta
+        new_height, new_width = int(0.8 * height), int(0.7 * width)
+        offset_height, offset_width = int(0.05 * height), int(0.04 * width)
+        meta.update(height=new_height, width=new_width)
+        with rasterio.open(offset_filename, "w", **meta) as sat_image:
+            for i in range(3):
+                sat_image.write_band(
+                    i + 1,
+                    data[
+                        i,
+                        offset_height : offset_height + new_height,
+                        offset_width : offset_width + new_width,
+                    ],
+                )
+
+
+if __name__ == "__main__":
+    shift_given_image()

409aa219fca216af1b7f5016821381cb648032c8
vasil <vaskonikolov2003@gmail.com>
2022-09-04 11:27:14 +0300

    add script to transfer files to /work/transfer/

--- src/communications/downlink.py
+++ src/communications/downlink.py
@@ -1,3 +1,17 @@
+import datetime
+from subprocess import run
+
 def send_message_down(message: str):
-    # TODO implement it better
+    print("transfering files to /work/transfer")
+    time = datetime.datetime.now()
+    time_date, time_time = str(time).split(" ") 
+    time = f"{time_date}T{time_time}"
+    time = time.replace(":", "_")
+    time = time.replace(".", "_")
+    print(time)
+    filename = f"transfer_file_{time}.txt"
+    with open(filename, "w") as file:
+        file.write(message)
+    run(["./src/transfer_data.sh", filename])
+
     print(message)
